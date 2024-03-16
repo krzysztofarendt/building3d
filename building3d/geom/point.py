9d2b45d6bae01796f4b62428cd3a9099b818c4fe
krzysztofarendt <16005748+krzysztofarendt@users.noreply.github.com>
2024-03-16 11:03:19 +0100

    Point add to a vector

--- building3d/geom/point.py
+++ building3d/geom/point.py
@@ -33,4 +33,7 @@ class Point:
             return False
 
     def __add__(self, other):
-        return Point(self.x + other.x, self.y + other.y, self.z + other.z)
+        if len(other) == 3:
+            return Point(self.x + other[0], self.y + other[1], self.z + other[2])
+        else:
+            raise TypeError("Point can be added only with a vector of length 3")
