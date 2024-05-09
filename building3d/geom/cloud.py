f8a13fe796c51085bd54ff5fa7a848798719b525
krzysztofarendt <16005748+krzysztofarendt@users.noreply.github.com>
2024-05-09 08:45:49 +0200

    Add unit test for are_points_in_set

--- building3d/geom/cloud.py
+++ building3d/geom/cloud.py
@@ -1,7 +1,8 @@
 from building3d.geom.point import Point
 
 
-def are_points_in_set(pts: list[Point], are_in: list[Point]):
+def are_points_in_set(pts: list[Point], are_in: list[Point]) -> bool:
+    """Test if points `pts` are a subset of `are_in`."""
     pts_set = set(pts)
     are_in_set = set(are_in)
     return pts_set.issubset(are_in_set)
---
+++ tests/test_cloud.py
@@ -0,0 +1,13 @@
+from building3d.geom.point import Point
+from building3d.geom.cloud import are_points_in_set
+
+
+def test_are_points_in_set():
+    p0 = Point(0.0, 0.0, 0.0)
+    p1 = Point(1.0, 1.0, 1.0)
+    p2 = Point(2.0, 2.0, 2.0)
+    p3 = Point(3.0, 3.0, 3.0)
+    cloud1 = [p0, p1]
+    cloud2 = [p0, p1, p2, p3]
+    assert are_points_in_set(cloud1, cloud2)
+    assert not are_points_in_set(cloud2, cloud1)
