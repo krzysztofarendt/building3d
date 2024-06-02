5b8040d08ed2b2c09125edbd843510faf7e03a26
krzysztofarendt <16005748+krzysztofarendt@users.noreply.github.com>
2024-06-02 10:49:30 +0200

    Add saving to B3D in example_3

--- examples/example_3.py
+++ examples/example_3.py
@@ -3,6 +3,8 @@ from building3d.display.plot_zone import plot_zone
 from building3d.geom.predefined.box import box
 from building3d.mesh.mesh import Mesh
 from building3d.mesh.quality.mesh_stats import mesh_stats
+from building3d.io.b3d import write_b3d
+from building3d.geom.building import Building
 
 if __name__ == "__main__":
     zone = box(2.0, 5.0, 3.0)
@@ -17,3 +19,12 @@ if __name__ == "__main__":
     # Plot
     plot_zone(zone)
     plot_mesh(mesh)
+
+    # Save to B3D
+    building = Building(name="example_3")
+    building.add_zone_instance(zone)
+
+    # The below line is needed because the mesh instance was created
+    # outside the building instance
+    building.mesh = mesh
+    write_b3d("example_3.b3d", building)
