3c7a956813c6b367698459a09b3a05c0a95f1ed3
krzysztofarendt <16005748+krzysztofarendt@users.noreply.github.com>
2024-06-08 13:46:29 +0200

    Reverse wall vertices order if normal vector is wrong

--- building3d/geom/predefined/floor_plan.py
+++ building3d/geom/predefined/floor_plan.py
@@ -1,8 +1,14 @@
-from building3d import random_id
+import logging
+
+import numpy as np
+
 from building3d.geom.point import Point
 from building3d.geom.polygon import Polygon
 from building3d.geom.wall import Wall
 from building3d.geom.zone import Zone
+
+
+logger = logging.getLogger(__name__)
 
 
 def floor_plan(
@@ -14,6 +20,10 @@ def floor_plan(
     name: str | None = None,
 ) -> Zone:
     """Make a zone from a floor plan (list of (x, y) points)."""
+
+    # TODO: translation
+    # TODO: rotation
+    # TODO: controllable wall names
 
     plan = [(float(x), float(y)) for x, y in plan]
 
@@ -37,18 +47,56 @@ def floor_plan(
         walls.append(wall)
 
     floor_poly = Polygon(floor_pts)
+    # Floor's normal should point downwards
+    if not np.isclose(floor_poly.normal, [0, 0, -1]).all():
+        floor_poly = floor_poly.flip()
+
     ceiling_poly = Polygon(ceiling_pts)
+    # Ceiling's normal should point upwards
+    if not np.isclose(ceiling_poly.normal, [0, 0, 1]).all():
+        ceiling_poly = ceiling_poly.flip()
 
     floor = Wall([floor_poly])
     ceiling = Wall([ceiling_poly])
+
+    # Make sure all polygon normals should point outwards the zone.
+    # It is done by comparing the order of wall vertices to the order
+    # of floor vertices - they should be opposite.
+    for k in range(len(walls)):
+        w_poly = walls[k].get_polygons()[0]  # There is only one
+        w_pts = w_poly.points
+        f_poly = floor.get_polygons()[0]  # There is only one
+        f_pts = f_poly.points
+
+        wall_z0_pts = []
+        for i in range(len(w_pts)):
+            if w_pts[i].z == 0:
+                wall_z0_pts.append(w_pts[i])
+
+        floor_adjacent_pts = []
+        for i in range(len(f_pts)):
+            if f_pts[i] in wall_z0_pts:
+                floor_adjacent_pts.append(f_pts[i])
+
+            if len(floor_adjacent_pts) == 2:
+                break
+
+        if wall_z0_pts[0] == floor_adjacent_pts[1] and wall_z0_pts[1] == floor_adjacent_pts[0]:
+            # Direction is OK
+            pass
+        else:
+            # Need to reverse the order of polygon vertices
+            w_poly = w_poly.flip()
+            old_wall_name = walls[k].name
+            walls[k] = Wall([w_poly])
+            logger.debug(
+                f"Wall vertices reversed. Old name: {old_wall_name}. New name: {walls[k].name}"
+            )
 
     walls.append(floor)
     walls.append(ceiling)
 
     zone = Zone(name=name)
-    # TODO: Assert all polygon normals are pointing outwards the zone
-    # TODO: Solid should find polygons with reversed order of vertices and fix it
     zone.add_solid(name=name, walls=walls)
 
     return zone
-
--- tests/test_floor_plan.py
+++ tests/test_floor_plan.py
@@ -1,17 +1,42 @@
+import numpy as np
+
 from building3d.geom.predefined.floor_plan import floor_plan
 from building3d.display.plot_objects import plot_objects
 
 
-def test_floor_plan():
-    plan = [(0, 0), (5, 0), (5, 5), (0, 2.5)]
+def test_floor_plan(show=False):
+    plan = [(0, 0), (5, 0), (5, 5), (0, 5)]
     h = 2
     zone = floor_plan(plan, height=h)
+    vol = list(zone.solids.values())[0].volume
+    assert np.isclose(vol, 5 * 5 * 2)
 
     wall_names = zone.get_wall_list()
     walls = [zone.get_wall(name) for name in wall_names]
 
-    plot_objects(*walls)
+    # TODO: Make sure normals are pointing outside the zone
+
+    if show:
+        plot_objects(*walls)
+
+
+def test_floor_plan_reversed(show=False):
+    plan = [(0, 0), (5, 0), (5, 5), (0, 5)]
+    plan = plan[::-1]
+    h = 2
+    zone = floor_plan(plan, height=h)
+    vol = list(zone.solids.values())[0].volume
+    assert np.isclose(vol, 5 * 5 * 2)
+
+    wall_names = zone.get_wall_list()
+    walls = [zone.get_wall(name) for name in wall_names]
+
+    # TODO: Make sure normals are pointing outside the zone
+
+    if show:
+        plot_objects(*walls)
 
 
 if __name__ == "__main__":
-    test_floor_plan()
+    test_floor_plan(show=True)
+    test_floor_plan_reversed(show=True)
