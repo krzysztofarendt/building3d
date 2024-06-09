33d49c749426e0b3eb08ae18cd7619fc66c3edb4
krzysztofarendt <16005748+krzysztofarendt@users.noreply.github.com>
2024-06-09 08:56:13 +0200

    Add size argument to random_id()

--- building3d/__init__.py
+++ building3d/__init__.py
@@ -3,8 +3,23 @@ import uuid
 import numpy as np
 
 
-def random_id() -> str:
-    return str(uuid.uuid4())
+def random_id(size: int | None = None) -> str:
+    """Return UUID or a random string.
+
+    If `size` is given, the hyphens are removed from the UUID
+    and the first `size` characters are returned.
+    If `size` is `None`, the full UUID is returned.
+
+    When `size` is given, the maximum length of the output is 32.
+    When `size` is `None`, the maximum length of the output is 36,
+    because UUIDs have 4 additional hyphens.
+    """
+    uid = str(uuid.uuid4())
+    if size is None:
+        return uid
+    else:
+        uid = uid.replace("-", "")
+        return uid[:size]
 
 
 def random_within(lim=1.0):
