58f79c26f43b287566c84a5c9bef66b5a4c1b4f0
krzysztofarendt <16005748+krzysztofarendt@users.noreply.github.com>
2024-06-15 10:16:45 +0200

    Complete docstring for Polygon.is_facing_polygon()

--- building3d/geom/polygon.py
+++ building3d/geom/polygon.py
@@ -648,19 +648,22 @@ class Polygon:
         else:
             return False
 
-    def is_facing_polygon(self, poly, exact=True) -> bool:
+    def is_facing_polygon(self, poly, exact: bool = True) -> bool:
         """Checks if this polygon is facing another polygon.
 
         Returns True if all points of two polygons are equal and their normals
         are pointing towards each other.
 
         If exact is True, all points of two polygons must be equal (order may be different).
-        If exact is False, the methods checks only in points are coplanar and
+        If exact is False, the method checks only in points are coplanar and
         normal vectors are opposite.
 
         Args:
             poly: another polygon
             exact: if True, all points of adjacent polygons must be equal
+
+        Return:
+            True if the polygons are facing each other
         """
         if exact:
             this_points = set(self.points)
