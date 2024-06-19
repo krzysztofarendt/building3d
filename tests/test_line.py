3f4a2e4e0b69b1beb02d9c0fdff9bb0a2ed83ba7
krzysztofarendt <16005748+krzysztofarendt@users.noreply.github.com>
2024-06-19 08:17:20 +0200

    Add line_intersection and line_segment_intersection

--- tests/test_line.py
+++ tests/test_line.py
@@ -3,6 +3,9 @@ import numpy as np
 from building3d.geom.line import create_point_between_2_points_at_distance
 from building3d.geom.line import create_points_between_2_points
 from building3d.geom.line import distance_point_to_edge
+from building3d.geom.line import is_point_on_segment
+from building3d.geom.line import line_intersection
+from building3d.geom.line import line_segment_intersection
 from building3d.geom.point import Point
 
 
@@ -49,3 +52,100 @@ def test_distance_point_to_edge():
     assert np.isclose(d, np.sqrt(2))
     d = distance_point_to_edge(ptest5, p1, p2)
     assert np.isclose(d, 0)
+
+
+def test_is_point_on_segment():
+    pt2 = Point(2.0, 0.0, 0.0)
+    pt1 = Point(0.0, 0.0, 0.0)
+
+    ptest = Point(1.0, 0.0, 0.0)
+    assert is_point_on_segment(ptest, pt1, pt2) is True
+    ptest = Point(1.0, 1.0, 0.0)
+    assert is_point_on_segment(ptest, pt1, pt2) is False
+    ptest = Point(2.0, 0.0, 0.0)
+    assert is_point_on_segment(ptest, pt1, pt2) is True
+    ptest = Point(0.0, 0.0, 0.0)
+    assert is_point_on_segment(ptest, pt1, pt2) is True
+    ptest = Point(-0.001, 0.0, 0.0)
+    assert is_point_on_segment(ptest, pt1, pt2) is False
+
+
+def test_line_intersection():
+    p1 = Point(0.0, 0.0, 0.0)
+    d1 = np.array([0.0, 1.0, 0.0])
+    p2 = Point(1.0, 2.0, 0.0)
+    d2 = np.array([-0.1, 0.0, 0.0])
+    ptest = line_intersection(p1, d1, p2, d2)
+    assert ptest == Point(0.0, 2.0, 0.0)
+
+    p1 = Point(0.0, 0.0, 0.0)
+    d1 = np.array([0.0, 1.0, 0.0])
+    p2 = Point(2.0, 2.0, 0.0)
+    d2 = np.array([-0.1, 0.0, 0.0])
+    ptest = line_intersection(p1, d1, p2, d2)
+    assert ptest == Point(0.0, 2.0, 0.0)
+
+    p1 = Point(0.0, 0.0, 0.0)
+    d1 = np.array([0.0, 1.0, 0.0])
+    p2 = Point(2.0, 2.0, 0.0)
+    d2 = np.array([0.0, 1.0, 0.0])
+    ptest = line_intersection(p1, d1, p2, d2)
+    assert ptest is None
+
+    p1 = Point(0.0, 0.0, 0.0)
+    d1 = np.array([0.0, 1.0, 0.0])
+    p2 = Point(2.0, 2.0, 0.0)
+    d2 = np.array([0.0, 0.0, 0.0])
+    ptest = line_intersection(p1, d1, p2, d2)
+    assert ptest is None
+
+    p1 = Point(0.0, 0.0, 0.0)
+    d1 = np.array([0.0, 1.0, 0.0])
+    p2 = Point(0.0, 0.0, 0.0)
+    d2 = np.array([0.0, 1.0, 0.0])
+    ptest = line_intersection(p1, d1, p2, d2)
+    assert ptest is None
+
+
+def test_line_segment_intersection():
+    pa1 = Point(0.0, 0.0, 0.0)
+    pb1 = Point(2.0, 0.0, 0.0)
+    pa2 = Point(1.0, 1.0, 0.0)
+    pb2 = Point(1.0, -1.0, 0.0)
+    ptest = line_segment_intersection(pa1, pb1, pa2, pb2)
+    assert ptest == Point(1.0, 0.0, 0.0)
+
+    pa1 = Point(0.0, 0.0, 0.0)
+    pb1 = Point(2.0, 0.0, 0.0)
+    pa2 = Point(0.0, 1.0, 0.0)
+    pb2 = Point(0.0, -1.0, 0.0)
+    ptest = line_segment_intersection(pa1, pb1, pa2, pb2)
+    assert ptest == Point(0.0, 0.0, 0.0)
+
+    pa1 = Point(0.0, 0.0, 0.0)
+    pb1 = Point(2.0, 0.0, 0.0)
+    pa2 = Point(0.0, 0.0, 0.0)
+    pb2 = Point(0.0, 1.0, 0.0)
+    ptest = line_segment_intersection(pa1, pb1, pa2, pb2)
+    assert ptest == Point(0.0, 0.0, 0.0)
+
+    pa1 = Point(0.0, 0.0, 0.0)
+    pb1 = Point(2.0, 0.0, 0.0)
+    pa2 = Point(0.0, 0.0, 0.0)
+    pb2 = Point(-2.0, 0.0, 0.0)
+    ptest = line_segment_intersection(pa1, pb1, pa2, pb2)
+    assert ptest is None  # Because same directions
+
+    pa1 = Point(0.0, 0.0, 0.0)
+    pb1 = Point(2.0, 0.0, 0.0)
+    pa2 = Point(1.0, 0.0, 0.0)
+    pb2 = Point(-2.0, 0.0, 0.0)
+    ptest = line_segment_intersection(pa1, pb1, pa2, pb2)
+    assert ptest is None  # Because same directions
+
+    pa1 = Point(0.0, 0.0, 0.0)
+    pb1 = Point(2.0, 0.0, 0.0)
+    pa2 = Point(1.0, 1.0, 1.0)
+    pb2 = Point(1.0, -1.0, 1.0)
+    ptest = line_segment_intersection(pa1, pb1, pa2, pb2)
+    assert ptest is None
