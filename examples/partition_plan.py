d64009ad656ff851cc51200e46ad1c6a29958fc5
krzysztofarendt <16005748+krzysztofarendt@users.noreply.github.com>
2024-07-03 08:59:35 +0200

    Add partition_plan()
    Return a list of vertical walls based on a list of x, y points and height.
    
    This function can be used to create internal walls for a solid (partitions)
    or to create external shading elements.

---
+++ examples/partition_plan.py
@@ -0,0 +1,12 @@
+from building3d.geom.predefined.walls.partition_plan import partition_plan
+from building3d.display.plot_objects import plot_objects
+
+
+if __name__ == "__main__":
+    walls = partition_plan(
+        plan = [(0, 0), (5, 0), (5, 5), (10, 5)],
+        height = 2.5,
+        translate = (0.0, 0.0, 0.0),
+        rot_angle = 0.0,
+    )
+    plot_objects(*walls)
