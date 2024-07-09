19bac625dd9d51395d5b6e4b07a33a847cb1dcaa
krzysztofarendt <16005748+krzysztofarendt@users.noreply.github.com>
2024-07-09 20:24:46 +0200

    Added casting from recursive default dict to dict

--- building3d/types/recursive_default_dict.py
+++ building3d/types/recursive_default_dict.py
@@ -1,6 +1,18 @@
+import json
+
 from collections import defaultdict
 
 
 def recursive_default_dict():
     """Default dict with default dict as the default type for all nested levels."""
     return defaultdict(recursive_default_dict)
+
+
+def recursive_default_dict_to_dict(rdd):
+    """Convert recursive default dict to standard dict.
+
+    Uses the `json` module, so does not support classes and functions as keys and values.
+    """
+    d = json.loads(json.dumps(rdd))
+    return d
+
