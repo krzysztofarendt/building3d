ca2d39dd31af9be302968a48b69682710a65afcf
Krzysztof Arendt <16005748+krzysztofarendt@users.noreply.github.com>
2024-07-25 18:56:05 +0200

    Switch off plotting by default in ray simulator test

--- tests/rays/test_raysimulator.py
+++ tests/rays/test_raysimulator.py
@@ -9,7 +9,7 @@ from building3d.geom.point import Point
 from building3d.simulators.rays.simulator import RaySimulator
 
 
-def test_ray_simulator():
+def test_ray_simulator(plot=False):
     L = 1
     W = 1
     H = 1
@@ -34,7 +34,10 @@ def test_ray_simulator():
     building = Building(name="building")
     building.add_zone(zone)
 
-    movie_path = "tmp/test_ray_simulator.mp4"
+    if plot:
+        movie_path = "tmp/test_ray_simulator.mp4"
+    else:
+        movie_path = None
 
     raysim = RaySimulator(
         building = building,
@@ -55,4 +58,6 @@ def test_ray_simulator():
     assert len(unique_locations) == 3, \
         "Rays not in all three solids at the end of simulation?"
 
-    assert os.path.exists(movie_path)
+    if plot:
+        assert isinstance(movie_path, str)
+        assert os.path.exists(movie_path)
