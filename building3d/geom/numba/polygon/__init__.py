d63f60622f29845c8bff906c7916c5f21fa8d0f8
Krzysztof Arendt <16005748+krzysztofarendt@users.noreply.github.com>
2024-08-11 10:17:09 +0200

    Add Polygon.copy()

--- building3d/geom/numba/polygon/__init__.py
+++ building3d/geom/numba/polygon/__init__.py
@@ -1,3 +1,5 @@
+import logging
+
 from building3d import random_id
 from building3d.geom.paths.validate_name import validate_name
 from building3d.geom.numba.types import PointType, VectorType, IndexType
@@ -5,6 +7,10 @@ from building3d.geom.numba.points import are_points_coplanar
 from building3d.geom.numba.vectors import normal
 from building3d.geom.numba.triangles import triangulate
 from building3d.geom.numba.polygon.centroid import polygon_centroid
+from building3d.geom.numba.polygon.area import polygon_area
+
+
+logger = logging.getLogger(__name__)
 
 
 class Polygon:
@@ -40,3 +46,16 @@ class Polygon:
             self.tri: IndexType = triangles
 
         self.ctr = polygon_centroid(self.pts, self.tri)
+        self.area = polygon_area(self.pts, self.vn)
+
+    def copy(self, new_name: str | None = None):
+        """Return a copy of itself (with a new name).
+
+        Args:
+            new_name: polygon name (must be unique within a Wall)
+
+        Return:
+            Polygon
+        """
+        logger.debug(f"Creating a copy of the polygon {self}. New name = {new_name}")
+        return Polygon(self.pts, name=new_name)
--- tests/numba/test_polygon.py
+++ tests/numba/test_polygon.py
@@ -49,7 +49,6 @@ def test_polygon_area():
     vn = new_vector(0, 0, 1)
     area = polygon_area(pts, vn)
     assert np.isclose(area, 1)
-    breakpoint()
 
 
 def test_polygon():
@@ -64,9 +63,22 @@ def test_polygon():
     assert len(poly.tri) == 2
     assert len(poly.pts) == 4
     assert np.allclose(poly.vn, [0, 0, 1])
+    assert np.isclose(poly.area, 1)
     # Check if uid is random
     for _ in range(10):
         poly1 = Polygon(pts)
         poly2 = Polygon(pts)
         assert poly1.name != poly2.name
         assert poly1.uid != poly2.uid
+
+def test_polygon_copy():
+    pt0 = new_point(0, 0, 0)
+    pt1 = new_point(1, 0, 0)
+    pt2 = new_point(1, 1, 0)
+    pt3 = new_point(0, 1, 0)
+    pts = np.vstack((pt0, pt1, pt2, pt3))
+    poly1 = Polygon(pts, name="poly1")
+    poly2 = poly1.copy("poly2")
+    assert np.allclose(poly1.pts, poly2.pts)
+    assert poly1 is not poly2.pts
+    assert poly1.uid != poly2.uid
