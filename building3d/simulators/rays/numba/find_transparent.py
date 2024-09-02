066d4be4f2abb069e0c4c362463b41e29e847b60
Krzysztof Arendt <16005748+krzysztofarendt@users.noreply.github.com>
2024-09-02 07:59:43 +0200

    Add numba ray update_location()

--- building3d/simulators/rays/numba/find_transparent.py
+++ building3d/simulators/rays/numba/find_transparent.py
@@ -1,12 +1,19 @@
 from building3d.geom.numba.building import Building
 from building3d.geom.numba.building.graph import graph_polygon
 from building3d.geom.paths import PATH_SEP
+from building3d.geom.paths.object_path import split_path
 
 
 def find_transparent(building: Building) -> list[str]:
-    """Find and return the list of transparent polygons in the building.
+    """Finds and returns the list of transparent polygons in the building.
 
     A polygon is transparent if it separates two adjacent solids within a single zone.
+
+    Args:
+        building: Building instance
+
+    Returns:
+        list of paths to polygons
     """
     # Find facing polygons (matching exactly)
     graph = graph_polygon(building, facing=True, overlapping=False, touching=False)
@@ -19,8 +26,8 @@ def find_transparent(building: Building) -> list[str]:
         assert len(v) == 1, f"Expected one facing polygon, but found {len(v)}"
 
         if k not in added or v[0] not in added:
-            bdg0, z0, sld0, wll0, plg0 = k.split(PATH_SEP)
-            bdg1, z1, sld1, wll1, plg1 = v[0].split(PATH_SEP)
+            bdg0, z0, sld0, wll0, plg0 = split_path(k)
+            bdg1, z1, sld1, wll1, plg1 = split_path(v[0])
 
             # Doesn't have to check if plg0 is facing plg1,
             # because if they are in the graph, they must be
--- building3d/simulators/rays/numba/ray.py
+++ building3d/simulators/rays/numba/ray.py
@@ -6,12 +6,14 @@ import numpy as np
 from building3d.geom.numba.points import new_point
 from building3d.geom.numba.polygon import Polygon
 from building3d.geom.numba.building import Building
+from building3d.geom.numba.building.find_location import find_location
 from building3d.geom.numba.vectors import normal, new_vector
 from building3d.geom.paths import PATH_SEP
 from building3d.geom.numba.types import PointType
 from .find_transparent import find_transparent
-# from .find_target import find_target
-# from .find_transparent import find_transparent
+from .find_target import find_target
+from building3d.geom.paths.object_path import object_path, split_path
+from building3d.simulators.rays.numba.find_transparent import find_transparent
 # from .get_property import get_property
 from .config import RAY_LINE_LEN
 
@@ -20,7 +22,7 @@ logger = logging.getLogger(__name__)
 
 
 class Ray:
-    buffer_size: int = RAY_LINE_LEN  # Used only for plotting
+    buff_size: int = RAY_LINE_LEN  # Used only for plotting
     transparent = []
     transparent_checked = False
 
@@ -34,21 +36,52 @@ class Ray:
         building: Building,
         properties: None | dict = None,
     ):
+        # Position
         self.pos = position
+        # Building instance
         self.bdg = building
+        # Velocity
+        self.vel = new_vector(0.0, 0.0, 0.0)
+        # Energy
+        self.enr = 1.0
 
+        # Current location (path to solid)
+        self.loc: str = ""
+        # Target surface path
+        self.trg_surf: str = ""
+        # Target surface absorption
+        self.trg_absorption: float = 0.0
+
+        # Distance to target surface (current, from last step, increment)
+        self.dist = np.inf
+        self.dist_prev = np.inf
+        self.dist_inc = 0
+
+        # Simulation step
+        self.num_step = 0
+        # Number of steps after touching last surface
+        self.num_steps_after_contact = 0
+
+        # If True, will stop this ray (end of simulation)
+        self.stop = False
+
+        # Wall properties
         if isinstance(properties, dict):
             self.prop = properties
         else:
             self.prop = Ray.default_properties(self.bdg)
 
-        self.vel = new_vector(0.0, 0.0, 0.0)
-        self.enr = 1.0
+        # Initialize buffer of previous positions
+        self.past_pos = deque(maxlen=Ray.buff_size)
+        for _ in range(Ray.buff_size):
+            self.past_pos.appendleft(self.pos)
 
+        # Find transparent surfaces
         if not Ray.transparent_checked:
             Ray.transparent = find_transparent(self.bdg)
+            Ray.transparent_checked = True
 
-        ...
+        logger.debug(f"Ray created: {self}")
 
     @staticmethod
     def default_properties(building: Building):
@@ -64,3 +97,24 @@ class Ray:
 
         return default
 
+    def update_location(self):
+        if self.enr <= 0:
+            return
+
+        logger.debug(f"Update location of {self}")
+
+        # If target surface and/or last known solid are known, start searching with them
+        # Otherwise search in unknown order
+        first_look_at = []
+
+        if len(self.loc) > 0:
+            first_look_at.append(self.loc)
+
+        if len(self.trg_surf) > 0:
+            z, s, _, _ = split_path(self.trg_surf)
+            trg_sld = object_path(zone=z, solid=s)
+            first_look_at.append(trg_sld)
+
+        self.loc = find_location(self.pos, self.bdg, *first_look_at)
+
+        logger.debug(f"Update ray location to: {self.loc}")
