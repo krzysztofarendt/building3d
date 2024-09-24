f39a74baf40cd7da3bed503b338aa57cfc29905e
krzysztofarendt <16005748+krzysztofarendt@users.noreply.github.com>
2024-09-24 22:32:34 +0200

    Add bvh.py

---
+++ building3d/simulators/rays_numba/bvh.py
@@ -0,0 +1,141 @@
+from numba import njit
+import numpy as np
+
+from building3d.geom.types import FLOAT, INT, PointType, IndexType
+from building3d.geom.polygon.crossing import is_line_segment_crossing_polygon
+
+
+@njit
+def make_bvh_grid(
+    min_xyz: tuple[float, float, float],
+    max_xyz: tuple[float, float, float],
+    poly_pts: list[PointType],
+    poly_tri: list[IndexType],
+    step: float = 1.0,
+) -> dict[tuple[int, int, int], set[int]]:
+    """Create a Bounding Volume Hierarchy (BVH) grid for faster collision detection.
+
+    This function divides the space defined by min_xyz and max_xyz into a grid of cubes,
+    and associates each polygon with the grid cells it intersects or is contained within.
+
+    Args:
+        min_xyz (tuple[float, float, float]): Minimum coordinates (x, y, z) of the bounding box.
+        max_xyz (tuple[float, float, float]): Maximum coordinates (x, y, z) of the bounding box.
+        poly_pts (list[PointType]): List of points for each polygon.
+        poly_tri (list[IndexType]): List of triangle indices for each polygon.
+        step (float, optional): Size of each grid cell. Defaults to 1.0.
+
+    Returns:
+        dict[tuple[int, int, int], set[int]]: A dictionary where keys are grid
+        cell coordinates and values are sets of polygon indices that intersect
+        with or are contained within each cell
+    """
+    min_x, min_y, min_z = min_xyz
+    max_x, max_y, max_z = max_xyz
+    grid = {}
+
+    x = min_x
+    y = min_y
+    z = min_z
+
+    keys = []
+    while z <= max_z:
+        keys.append((int(x / step), int(y / step), int(z / step)))
+        if x < max_x:
+            x += step
+        else:
+            x = min_x
+            if y < max_y:
+                y += step
+            else:
+                z += step
+
+    for key in keys:
+        # TODO: replace grid values with sets once Numba supports them as values of typed dicts
+        # https://numba.pydata.org/numba-doc/dev/reference/pysupported.html#typed-dict
+        polynums = []
+        added = set([0])  # Hint for type inference
+        added.remove(0)
+
+        for pn, (pts, tri) in enumerate(zip(poly_pts, poly_tri)):
+            min_xyz = key
+            max_xyz = (key[0] + step, key[1] + step, key[2] + step)
+            if pn not in added:
+                if is_polygon_crossing_cube(pts, tri, min_xyz, max_xyz):
+                    polynums.append(pn)
+            grid[key] = np.array(polynums, dtype=INT)
+
+    return grid
+
+
+@njit
+def is_polygon_crossing_cube(
+    pts,
+    tri,
+    min_xyz,
+    max_xyz,
+) -> bool:
+    """Check if a polygon intersects with or is contained within a cube.
+
+    Args:
+        pts: List of points representing the polygon vertices.
+        tri: List of triangle indices defining the polygon's triangulation.
+        min_xyz (tuple): Minimum coordinates (x, y, z) of the cube.
+        max_xyz (tuple): Maximum coordinates (x, y, z) of the cube.
+
+    Returns:
+        bool: True if the polygon intersects with or is contained within the cube, False otherwise.
+    """
+    # Check if any polygon vertex is inside the cube
+    for pt in pts:
+        if (
+            pt[0] < max_xyz[0] and pt[1] < max_xyz[1] and pt[2] < max_xyz[2] and
+            pt[0] >= min_xyz[0] and pt[1] >= min_xyz[1] and pt[2] >= min_xyz[2]
+        ):
+            return True
+
+    # Check if any of the cube edges crosses the polygon
+    edges = cube_edges(min_xyz, max_xyz)
+
+    for i in range(len(edges)):
+        seg_start = edges[i, 0]
+        seg_end = edges[i, 1]
+        if is_line_segment_crossing_polygon(seg_start, seg_end, pts, tri):
+            return True
+
+    return False
+
+
+@njit
+def cube_edges(
+    min_xyz: tuple[FLOAT, FLOAT, FLOAT],
+    max_xyz: tuple[FLOAT, FLOAT, FLOAT],
+) -> PointType:
+    """Generate the edges of a cube defined by its minimum and maximum coordinates.
+
+    Args:
+        min_xyz (tuple[FLOAT, FLOAT, FLOAT]): The minimum x, y, z coordinates of the cube.
+        max_xyz (tuple[FLOAT, FLOAT, FLOAT]): The maximum x, y, z coordinates of the cube.
+
+    Returns:
+        Array containing 12 edges of the cube, where each edge is represented
+        as two points (start_point, end_point), and each point has
+        three coordinates (x, y, z). Shape = (12, 2, 3).
+    """
+    p0 = min_xyz
+    p1 = max_xyz
+    edges = np.array((
+        ((p0[0], p0[1], p0[2]), (p1[0], p0[1], p0[2])),  # Bottom front
+        ((p0[0], p0[1], p0[2]), (p0[0], p1[1], p0[2])),  # Bottom left
+        ((p0[0], p0[1], p0[2]), (p0[0], p0[1], p1[2])),  # Front left
+        ((p1[0], p0[1], p0[2]), (p1[0], p1[1], p0[2])),  # Bottom right
+        ((p1[0], p0[1], p0[2]), (p1[0], p0[1], p1[2])),  # Front right
+        ((p0[0], p1[1], p0[2]), (p1[0], p1[1], p0[2])),  # Bottom back
+        ((p0[0], p1[1], p0[2]), (p0[0], p1[1], p1[2])),  # Back left
+        ((p0[0], p0[1], p1[2]), (p1[0], p0[1], p1[2])),  # Top front
+        ((p0[0], p0[1], p1[2]), (p0[0], p1[1], p1[2])),  # Top left
+        ((p1[0], p1[1], p0[2]), (p1[0], p1[1], p1[2])),  # Back right
+        ((p1[0], p0[1], p1[2]), (p1[0], p1[1], p1[2])),  # Top right
+        ((p0[0], p1[1], p1[2]), (p1[0], p1[1], p1[2])),  # Top back
+    ))
+    return edges
--- building3d/simulators/rays_numba/simulation.py
+++ building3d/simulators/rays_numba/simulation.py
@@ -7,9 +7,13 @@ from building3d.geom.zone import Zone
 from building3d.geom.solid import Solid
 from building3d.geom.wall import Wall
 from building3d.geom.polygon import Polygon
-from building3d.geom.polygon.ispointinside import is_point_inside_projection
+from building3d.geom.polygon.distance import distance_point_to_polygon
 from building3d.geom.types import PointType, VectorType, IndexType, IntDataType, FLOAT, INT
+from building3d.geom.vectors import normal
 from building3d.display.plot_objects import plot_objects
+from building3d.io.arrayformat import get_polygon_points_and_faces
+from .bvh import make_bvh_grid
+from .find_target import find_target_surface
 from .find_transparent import find_transparent
 
 
@@ -61,7 +65,7 @@ class Simulation:
             walls = walls,
             solids = solids,
             zones = zones,
-            trans_poly_nums = trans_poly_nums,
+            transparent_polygons = trans_poly_nums,
         )
         ray_plotter = RayPlotter(ray_pos)
         colors = ([1.0, 1.0, 1.0], [1.0, 0.0, 0.0])
@@ -86,19 +90,44 @@ def simulation_loop(
     walls: IndexType,
     solids: IndexType,
     zones: IndexType,
-    trans_poly_nums: set[int],
+    transparent_polygons: set[int],
 ) -> tuple[PointType, IntDataType]:
     """Simulation loop compiled to machine code with Numba.
     """
-    hits = np.zeros(len(sinks))
-
     # Simulation parameters (TODO: add to config and/or property dict)
     t_step = 1e-4
     absorption = 0.1
     sink_radius = 0.1
 
-    # Initial energy
-    energy = np.ones(num_rays)
+    # Get polygon points and faces
+    poly_pts = []
+    poly_tri = []
+    num_polys = len(walls)
+    for pn in range(num_polys):
+        pts, tri = get_polygon_points_and_faces(points, faces, polygons, pn)
+        poly_pts.append(pts)
+        poly_tri.append(tri)
+
+    # Make BVH grid
+    grid_step = 0.5
+    min_x = points[:, 0].min()
+    min_y = points[:, 1].min()
+    min_z = points[:, 2].min()
+    max_x = points[:, 0].max()
+    max_y = points[:, 1].max()
+    max_z = points[:, 2].max()
+
+    grid = make_bvh_grid(
+        min_xyz = (min_x, min_y, min_z),
+        max_xyz = (max_x, max_y, max_z),
+        poly_pts = poly_pts,
+        poly_tri = poly_tri,
+        step = grid_step,
+    )
+
+    # Initial ray energy and received energy
+    energy = np.ones(num_rays, dtype=FLOAT)
+    hits = np.zeros(len(sinks), dtype=FLOAT)
 
     # Direction and velocity
     speed = 343.
@@ -107,6 +136,7 @@ def simulation_loop(
         direction[i] /= np.linalg.norm(direction[i])
     velocity = direction * speed
     delta_pos = velocity * t_step
+    reflection_dist = speed * t_step * 2.0
 
     # Initial position
     pos = np.zeros((num_rays, 3))
@@ -120,17 +150,25 @@ def simulation_loop(
         sink_dist[sn, :] = np.sqrt(np.sum((pos - sinks[sn])**2, axis=1))
 
     # Target surfaces
-    target_surfs: IndexType = find_target_surfaces(
-        pos,
-        direction,
-        points,
-        faces,
-        polygons,
-        walls,
-        solids,
-        zones,
-        trans_poly_nums,
-    )
+    target_surfs = np.full(num_rays, 1, dtype=INT)
+
+    for rn in range(num_rays):
+        x = int(pos[rn][0] / grid_step)
+        y = int(pos[rn][1] / grid_step)
+        z = int(pos[rn][2] / grid_step)
+        polygons_to_check = grid[(x, y, z)]
+
+        target_surfs[rn] = find_target_surface(
+            pos[rn],
+            direction[rn],
+            poly_pts,
+            poly_tri,
+            walls,
+            solids,
+            zones,
+            transparent_polygons,
+            polygons_to_check,
+        )
 
     # Move rays
     for i in range(num_steps):
@@ -148,67 +186,24 @@ def simulation_loop(
                     hits[sn] += energy[rn]
                     energy[rn] = 0.0
 
+            # Check near polygons
+            x = int(pos[rn][0] / grid_step)
+            y = int(pos[rn][1] / grid_step)
+            z = int(pos[rn][2] / grid_step)
+            # near_polygons = grid[(x, y, z)]
+            # min_dist = np.inf
+            nearest = -1
+            # for pn in near_polygons:
+            #     pts = poly_pts[pn]
+            #     tri = poly_tri[pn]
+            #     vn = normal(pts[-1], pts[0], pts[1])
+            #     dist = distance_point_to_polygon(pos[rn], pts, tri, vn)
+
+            #     if dist < reflection_dist and dist < min_dist:
+            #         min_dist = dist
+            #         nearest = pn
+
             # Reflections
             ...
 
     return pos, hits
-
-
-@njit
-def find_target_surfaces(
-    # Rays
-    pos: PointType,
-    direction: VectorType,
-    # Building
-    points: PointType,
-    faces: IndexType,
-    polygons: IndexType,
-    walls: IndexType,
-    solids: IndexType,
-    zones: IndexType,
-    trans_poly_nums: set[int],
-) -> IndexType:
-    pts, tri = get_polygon_pts_and_tri(0, points, faces, polygons)  # WIP
-    breakpoint()
-    pass
-
-
-@njit
-def get_polygon_pts_and_tri(
-    poly_num,
-    points,
-    faces,
-    polygons,
-) -> tuple[PointType, IndexType]:
-    """Returns polygon points and faces for a given polygon number.
-    """
-    # Get faces of the selected polygon
-    poly_faces = faces[polygons == poly_num]
-
-    # Point indices needs to be renumbered, a mapping from old to new numbers is needed
-    face_remap = {}
-
-    # Point numbers - this list is used to collect the needed points
-    pt_nums = []
-
-    new_index = 0
-    for old_index in poly_faces.flatten():
-        if old_index not in face_remap:  # We don't want to take a point index twice
-            face_remap[old_index] = new_index
-            pt_nums.append(old_index)
-            new_index += 1
-
-    # Collect the points
-    num_points = len(pt_nums)
-    poly_points = np.zeros((num_points, 3), dtype=FLOAT)
-
-    for i in range(num_points):
-        poly_points[i] = points[pt_nums[i]]
-
-    # Re-number face point indices
-    new_faces = np.zeros_like(poly_faces, dtype=INT)
-    for fn, fc in enumerate(poly_faces):
-        for pn in range(3):
-            new_faces[fn, pn] = face_remap[fc[pn]]
-
-    return poly_points, new_faces
