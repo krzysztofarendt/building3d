528cfd3f778d540db4c953cb5d13f2f828899f84
Krzysztof Arendt <16005748+krzysztofarendt@users.noreply.github.com>
2024-10-18 21:24:45 +0200

    Remove f-strings

--- building3d/geom/solid/box.py
+++ building3d/geom/solid/box.py
@@ -48,19 +48,19 @@ def box(
     p6 = new_point(1.0, 1.0, 1.0) * stretch + mv
     p7 = new_point(0.0, 1.0, 1.0) * stretch + mv
 
-    poly_fl = Polygon(np.vstack((p0, p3, p2, p1)), name=f"floor")
-    poly_w0 = Polygon(np.vstack((p0, p1, p5, p4)), name=f"wall-0")
-    poly_w1 = Polygon(np.vstack((p1, p2, p6, p5)), name=f"wall-1")
-    poly_w2 = Polygon(np.vstack((p3, p7, p6, p2)), name=f"wall-2")
-    poly_w3 = Polygon(np.vstack((p0, p4, p7, p3)), name=f"wall-3")
-    poly_rf = Polygon(np.vstack((p4, p5, p6, p7)), name=f"roof")
+    poly_fl = Polygon(np.vstack((p0, p3, p2, p1)), name="floor")
+    poly_w0 = Polygon(np.vstack((p0, p1, p5, p4)), name="wall-0")
+    poly_w1 = Polygon(np.vstack((p1, p2, p6, p5)), name="wall-1")
+    poly_w2 = Polygon(np.vstack((p3, p7, p6, p2)), name="wall-2")
+    poly_w3 = Polygon(np.vstack((p0, p4, p7, p3)), name="wall-3")
+    poly_rf = Polygon(np.vstack((p4, p5, p6, p7)), name="roof")
 
-    wall_fl = Wall([poly_fl], name=f"floor")
-    wall_w0 = Wall([poly_w0], name=f"wall-0")
-    wall_w1 = Wall([poly_w1], name=f"wall-1")
-    wall_w2 = Wall([poly_w2], name=f"wall-2")
-    wall_w3 = Wall([poly_w3], name=f"wall-3")
-    wall_rf = Wall([poly_rf], name=f"roof")
+    wall_fl = Wall([poly_fl], name="floor")
+    wall_w0 = Wall([poly_w0], name="wall-0")
+    wall_w1 = Wall([poly_w1], name="wall-1")
+    wall_w2 = Wall([poly_w2], name="wall-2")
+    wall_w3 = Wall([poly_w3], name="wall-3")
+    wall_rf = Wall([poly_rf], name="roof")
 
     solid = Solid(
         walls=[wall_fl, wall_w0, wall_w1, wall_w2, wall_w3, wall_rf], name=name
