31e980d3b7cff0ee1cba764495d9b3b6be957eec
Krzysztof Arendt <16005748+krzysztofarendt@users.noreply.github.com>
2024-10-27 00:01:59 +0200

    Merge IO examples

--- examples/io_example.py
+++ examples/io_example.py
@@ -8,8 +8,15 @@ from building3d.io.dotbim import read_dotbim
 from building3d.io.dotbim import write_dotbim
 from building3d.io.stl import read_stl
 from building3d.io.stl import write_stl
+from building3d.io.arrayformat import from_array_format
+from building3d.io.arrayformat import to_array_format
 
 if __name__ == "__main__":
+    print(
+        "This example creates a 3-solid building,\n"
+        "saves it to various formats and recovers back,\n"
+        "each time plotting the building to confirm it looks the same."
+    )
     output_dir = "out/building_example"
 
     zone = Zone(
@@ -22,15 +29,25 @@ if __name__ == "__main__":
     )
 
     building = Building([zone], "building")
+    print("Original building")
+    plot_objects((building,))
 
     write_b3d(f"{output_dir}/building.b3d", building, parent_dirs=True)
     building = read_b3d(f"{output_dir}/building.b3d")
+    print("Saved and loaded from B3D")
     plot_objects((building,))
 
     write_dotbim(f"{output_dir}/building.bim", building, parent_dirs=True)
     building = read_dotbim(f"{output_dir}/building.bim")
+    print("Saved and loaded from .bim")
     plot_objects((building,))
 
     write_stl(f"{output_dir}/building.stl", building, parent_dirs=True)
     building = read_stl(f"{output_dir}/building.stl")
+    print("Saved and loaded from STL")
+    plot_objects((building,))
+
+    points, faces, polygons, walls, solids, zones = to_array_format(building)
+    building = from_array_format(points, faces, polygons, walls, solids, zones)
+    print("Saved and loaded from array format")
     plot_objects((building,))
