f54f0c62aabb55ac6a842fe12c89833980ca8be2
Krzysztof Arendt <16005748+krzysztofarendt@users.noreply.github.com>
2024-10-30 22:43:09 +0100

    Add test for simulation config

---
+++ tests/rays/test_simulation_config.py
@@ -0,0 +1,40 @@
+from building3d.geom.building import Building
+from building3d.geom.solid.box import box
+from building3d.geom.zone import Zone
+from building3d.sim.rays.simulation_config import SimulationConfig
+
+
+def test_simulation_config():
+    """
+
+    The polygon and wall names in each "box" are hardcoded:
+    - floor
+    - wall_0 (XZ at ymin)
+    - wall_1 (YZ at xmax)
+    - wall_2 (XZ at ymax)
+    - wall_3 (YZ at xmin)
+    - roof
+    """
+    solid_0 = box(1, 1, 1, (0, 0, 0), "s0")
+    solid_1 = box(1, 1, 1, (1, 0, 0), "s1")
+    solid_2 = box(1, 1, 1, (1, 1, 0), "s2")
+    zone = Zone([solid_0, solid_1, solid_2], "z")
+    building = Building([zone], "b")
+
+    sim_cfg = SimulationConfig(building)
+    default_absorption = sim_cfg.surfaces["absorption"]["default"]
+    sim_cfg.set_surface_param("absorption", "b/z/s0", 1.0, building)
+    assert sim_cfg.surfaces["absorption"]["b/z/s0/floor/floor"] == 1.0
+    assert sim_cfg.surfaces["absorption"]["b/z/s0/wall_0/poly_0"] == 1.0
+    assert sim_cfg.surfaces["absorption"]["b/z/s0/wall_1/poly_1"] == 1.0
+    assert sim_cfg.surfaces["absorption"]["b/z/s0/wall_2/poly_2"] == 1.0
+    assert sim_cfg.surfaces["absorption"]["b/z/s0/wall_3/poly_3"] == 1.0
+    assert sim_cfg.surfaces["absorption"]["b/z/s0/ceiling/ceiling"] == 1.0
+
+    sim_cfg.set_surface_param("absorption", "b/z/s1/wall_0", 2.0, building)
+    assert sim_cfg.surfaces["absorption"]["b/z/s1/floor/floor"] == default_absorption
+    assert sim_cfg.surfaces["absorption"]["b/z/s1/wall_0/poly_0"] == 2.0
+    assert sim_cfg.surfaces["absorption"]["b/z/s1/wall_1/poly_1"] == default_absorption
+    assert sim_cfg.surfaces["absorption"]["b/z/s1/wall_2/poly_2"] == default_absorption
+    assert sim_cfg.surfaces["absorption"]["b/z/s1/wall_3/poly_3"] == default_absorption
+    assert sim_cfg.surfaces["absorption"]["b/z/s1/ceiling/ceiling"] == default_absorption
